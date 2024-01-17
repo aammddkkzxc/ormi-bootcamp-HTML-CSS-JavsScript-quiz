c0d343db5e8147c19694a8bf7b7306f055e0a9a9
aammddkkzxc <ideal0427@naver.com>
2024-01-18 00:49:38 +0900

    feat: r

--- 1000thousand-page-quiz/c.js
+++ 1000thousand-page-quiz/c.js
@@ -8,18 +8,18 @@ const loading = document.querySelector(".result_loading");
 
 function calculator(){
     const fieldValue = document.querySelector("#field_value");
-    let timeValue = document.querySelector("#time_value");
-    let timeValue_let = Number(timeValue.value);
+    const timeValue = document.querySelector("#time_value");
+    const timeValue_let = Number(timeValue.value);
 
     const fieldResult = document.querySelector(".field_result");
     const timeResult = document.querySelector(".time_result");
 
     if(fieldValue.value == ""){
-        alert('입력되지 않았습니다.');
+        alert('분야가 입력되지 않았습니다.');
         fieldValue.focus();
         return false;
     } else if(timeValue.value == ""){
-        alert('입력되지 않았습니다.');
+        alert('시간이 입력되지 않았습니다.');
         timeValue.focus();
         return false;
     } else if(timeValue_let > 24) {
@@ -31,10 +31,10 @@ function calculator(){
     loading.style.display = 'flex';
 
     setTimeout(function(){
-        loading.style.display = 'none';
-        result.style.display = 'flex';
         fieldResult.innerText = fieldValue.value;
         timeResult.innerText = parseInt((10000/timeValue_let), 10);
+        loading.style.display = 'none';
+        result.style.display = 'flex';
     }, 1800);
 
 }
@@ -46,11 +46,11 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
-window.onclick() = function(event) {
-    if(event.target == modal) {
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
         closeModal();
     }
-}
+});
 
 function copyUrl() {
     let url = window.location.href;
